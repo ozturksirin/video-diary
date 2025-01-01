b8f7d12fda22839e921287f9fb1503b103f19ec0
Öztürk Şirin <ozturksirininfo@gmail.com>
2025-01-01 10:31:47 +0300

    Add NativeWind integration and update project configuration

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,36 +1,48 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import { Slot, Stack } from "expo-router"; // Slot ve Stack kullanımı
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import "react-native-reanimated"; // Animasyon desteği
+import "../global.css"; // Global CSS için NativeWind desteği
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { useColorScheme } from "@/hooks/useColorScheme";
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
+// SplashScreen'i gizlemeden önce bekler
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  // Tema seçimi (dark/light)
   const colorScheme = useColorScheme();
+
+  // Font yükleme
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
 
+  // Fontlar yüklendiğinde SplashScreen'i gizler
   useEffect(() => {
     if (loaded) {
       SplashScreen.hideAsync();
     }
   }, [loaded]);
 
+  // Fontlar yüklenmediğinde boş bir ekran döner
   if (!loaded) {
     return null;
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
+        {/* Ana sekme navigasyonu */}
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        {/* Hata durumunda açılan sayfa */}
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
